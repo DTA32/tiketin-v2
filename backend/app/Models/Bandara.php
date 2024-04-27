119e1378fb34c954efcc36c606ce5eede5876987
DTA32 <97021335+DTA32@users.noreply.github.com>
2024-04-27 22:21:59 +0700

    be: remove api prefix, fix file naming, cleanup

--- backend/app/Models/Bandara.php
+++ backend/app/Models/Bandara.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class bandara extends Model
+class Bandara extends Model
 {
     use HasFactory;
     protected $table = 'bandara';
